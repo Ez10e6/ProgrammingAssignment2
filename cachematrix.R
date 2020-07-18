36fa6a8677919f5fa24fed96e6fa78ce07b07a03
Ez10e6 <45690875+Ez10e6@users.noreply.github.com>
2020-07-18 15:53:51 +0200

    Added comments to makeCacheMatrix and cacheSolve code

--- cachematrix.R
+++ cachematrix.R
@@ -8,15 +8,15 @@
 ## inverse of the matrix and getinv gets the inverse that is stored.
 
 makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL
+        inv <- NULL ## set the inverse to null initially
         set <- function(y) {
-                x <<- y
-                inv <<- NULL
+                x <<- y         ## if a new matrix is given change x
+                inv <<- NULL    ## and reset the inverse
         }
-        get <- function() x
-        setinv <- function(inverse) inv <<- inverse
-        getinv <- function() inv
-        list(set = set, get = get,
+        get <- function() x    ##if get is called return x
+        setinv <- function(inverse) inv <<- inverse ## if setinv is called asign inv a given value inverse
+        getinv <- function() inv  ## if getinv is called, return the inverse
+        list(set = set, get = get, ##return the list with the functions
              setinv = setinv,
              getinv = getinv)
 }
@@ -27,12 +27,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        inv <- x$getinv()
-        if(!is.null(inv)) {
+        inv <- x$getinv() ## get the value of the inverse of x
+        if(!is.null(inv)) { ## if the value is not null then return the value
                 return(inv)
         }
-        mtrx <- x$get()
-        inv <- solve(mtrx, ...)
-        x$setinv(inv)
-        inv
+        mtrx <- x$get() ## if the inverse wasn't there, get the matrix
+        inv <- solve(mtrx, ...) ## get the inverse
+        x$setinv(inv) ## set the inverse
+        inv ## return the inverse
 }
